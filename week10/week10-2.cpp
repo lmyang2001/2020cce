f79dbd942047b0f1d115ef0ef28f0967d633411f
lmyang2001 <79677170+lmyang2001@users.noreply.github.com>
2021-04-30 11:29:23 +0800

    Update week10-2.cpp

--- week10/week10-2.cpp
+++ week10/week10-2.cpp
@@ -13,8 +13,8 @@ int main()
 	//	printf("%s\n",line);
 	N++;
 		}
-	printf("¦³´X´Ê¾ð? %d\n",N);
-	printf("====¤À¹j½u====\n");
+	printf("æœ‰å¹¾æ£µæ¨¹? %d\n",N);
+	printf("====åˆ†éš”ç·š=====\n");
 
 	}
 }
