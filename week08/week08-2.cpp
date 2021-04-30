1a7a77080f14f648149c3e578192efb480bf367e
lmyang2001 <79677170+lmyang2001@users.noreply.github.com>
2021-04-30 11:23:21 +0800

    Add files via upload

---
+++ week08/week08-2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char line[100][10];
+int compare(const void*p1,const void*p2 ){
+    return strcmp((char*)p1,(char*)p2);
+    }
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i=-;i<n;i++){
+    scanf("%s",line[i]);
+    }
+    qsort(line, n, 10, conpare);
+
+    for(int i=0;i<n;i++){
+    printf("%s\n",line[i]);
+    }
+}
