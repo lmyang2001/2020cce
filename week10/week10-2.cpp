ae6ec86b2e720f0f427934ae1f3d3a7a870f73e4
lmyang2001 <79677170+lmyang2001@users.noreply.github.com>
2021-04-30 11:04:43 +0800

    Add files via upload

---
+++ week10/week10-2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+char line[1000];
+int main()
+{
+	int T;
+	scanf("%d\n\n",&T);
+
+	for(int t=0;t<T;t++){
+		int N=0;
+		while(gets(line)!=NULL ){
+		if( strcmp(line," ")==0 ) break;
+	//	printf("%s\n",line);
+	N++;
+		}
+	printf("有幾棵樹? %d\n",N);
+	printf("====分隔線====\n");
+
+	}
+}
