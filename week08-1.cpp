fd6d7e5678228ba71576569acceef9707d7fa050
lmyang2001 <79677170+lmyang2001@users.noreply.github.com>
2021-04-30 11:14:32 +0800

    Add files via upload

---
+++ week08-1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+char line[100][10];
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i=-;i<n;i++){
+    scanf("%s",line[i]);
+    }
+    for(int i=0;i<n;i++){
+    printf("%s\n",line[i]);
+    }
+}
