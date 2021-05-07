db236da9fa18c17a2cfdf101007f7d2a666076c1
lmyang2001 <79677170+lmyang2001@users.noreply.github.com>
2021-05-07 10:55:37 +0800

    Add files via upload

---
+++ week11/week11-4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+typedef struct data{
+    int ans;
+    char c;
+}DATA;
+///struct data box;
+DATA box;
+int main()
+{
+  box.ans=1;
+  box.c='A';
+  printf("%c %d\n",box.c,box.ans);
+}
