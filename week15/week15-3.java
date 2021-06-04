04e66f298222ae84f6e7db83feae3bebd90d30af
lmyang2001 <79677170+lmyang2001@users.noreply.github.com>
2021-06-04 10:08:49 +0800

    Create week15-3.java

---
+++ week15/week15-3.java
@@ -0,0 +1,12 @@
+import processing.sound.*;
+SoundFile player;
+void setup(){
+  size(400,200);
+  player = new SoundFile(this,"BLACK.mp3"); ///音樂要拉進去Processing
+}
+void draw(){
+  background(184,156,225);
+}
+void mousePressed(){
+  player.play();
+}
